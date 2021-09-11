6c88c5d37d15433b32ef8a0b25e92d13f617070b
NerdyLucifer <ashwat.kumarsingh.met20@itbhu.ac.in>
2021-09-11 18:46:11 +0530

    1st commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import Navbar from "./components/Navbar.js";
+import "./App.css"
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from "./components/pages/Home.js";
+import Products from "./components/pages/Products.js";
+import Services from "./components/pages/Services.js";
+import SignUp from "./components/pages/SignUp.js";
+import Footer from "./components/Footer.js";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Navbar/> 
+        <Switch>
+          <Route path="/" exact>
+            <Home></Home>
+          </Route>
+          <Route path="/services" exact>
+            <Services/>
+          </Route>
+          <Route path="/products" exact>
+            <Products/>
+          </Route>
+          <Route path="/sign-up" exact>
+            <SignUp/>
+          </Route>
+        </Switch>
+        <Footer/>
+      </Router>
+    </>
   );
 }
-
+ 
 export default App;
